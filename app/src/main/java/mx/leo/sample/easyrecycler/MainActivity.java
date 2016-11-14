812512583ab87813f780ad52a267e257ada77210
leonel Mendez <Leo Mendez>
2016-11-13 23:27:05 -0600

    NEW: Added on item clicklistener

--- app/src/main/java/mx/leo/sample/easyrecycler/MainActivity.java
+++ app/src/main/java/mx/leo/sample/easyrecycler/MainActivity.java
@@ -1,11 +1,16 @@
 package mx.leo.sample.easyrecycler;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
-import java.util.ArrayList;
+import org.jetbrains.annotations.Nullable;
+
+import mx.leo.easyrecycler.util.RecyclerViewItemClickListener;
+import mx.leo.easyrecycler.util.extensions.RecyclerViewKt;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.sample_recycler);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.sample_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        SampleAdapter sampleAdapter = new SampleAdapter();
+        final SampleAdapter sampleAdapter = new SampleAdapter();
         recyclerView.setAdapter(sampleAdapter);
 
         sampleAdapter.addItem("hola");
         sampleAdapter.addItem("mundo");
 
-
+        RecyclerViewKt.OnItemClickListener(recyclerView, new RecyclerViewItemClickListener.OnItemClickListener() {
+            @Override
+            public void onClick(@Nullable View view, @Nullable Integer position) {
+                Log.d("item", sampleAdapter.getItems().get(position));
+            }
+        });
     }
 }
---
+++ easyrecycler/src/main/kotlin/mx/leo/easyrecycler/util/RecyclerViewItemClickListener.kt
@@ -0,0 +1,38 @@
+package mx.leo.easyrecycler.util
+
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+
+class RecyclerViewItemClickListener(context: Context, var recyclerViewItem: OnItemClickListener): RecyclerView.OnItemTouchListener{
+
+    lateinit var gestureDetector:GestureDetector
+    init {
+        gestureDetector = GestureDetector(context, object:GestureDetector.SimpleOnGestureListener(){
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                return true
+            }
+
+        })
+    }
+    override fun onTouchEvent(rv: RecyclerView?, e: MotionEvent?) {
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView?, e: MotionEvent?): Boolean {
+        val child:View? = rv?.findChildViewUnder(e!!.x,e.y)
+        if(gestureDetector.onTouchEvent(e)) {
+            recyclerViewItem.onClick(child, rv?.getChildAdapterPosition(child))
+        }
+        return false
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+    }
+
+    interface OnItemClickListener {
+        fun onClick(view: View?, position: Int?)
+    }
+}
