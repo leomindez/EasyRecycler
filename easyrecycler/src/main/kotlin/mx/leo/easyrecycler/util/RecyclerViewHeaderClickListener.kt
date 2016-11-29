8c64b18fceee7025be719d1f4c46e33465e86c95
leonel Mendez <Leo Mendez>
2016-11-29 01:23:35 -0600

    UPDATE: Fixed header item position issue

---
+++ easyrecycler/src/main/kotlin/mx/leo/easyrecycler/util/RecyclerViewHeaderClickListener.kt
@@ -0,0 +1,25 @@
+package mx.leo.easyrecycler.util
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.MotionEvent
+import android.view.View
+
+open class RecyclerViewHeaderClickListener(context: Context, var headerClick:OnHeaderClickListener,var itemClick: OnItemClickListener) : RecyclerViewItemClickListener(context,itemClick) {
+
+    override fun onInterceptTouchEvent(rv: RecyclerView?, e: MotionEvent?): Boolean {
+
+        val child:View? = rv?.findChildViewUnder(e!!.x,e.y)
+        if(gestureDetector.onTouchEvent(e)) {
+            if(rv?.getChildAdapterPosition(child) == 0)
+                headerClick.onHeaderClick()
+            else
+                recyclerViewItem.onItemClick(child, (rv?.getChildAdapterPosition(child)?.minus(1)))
+        }
+        return false
+    }
+
+    interface OnHeaderClickListener {
+        fun onHeaderClick()
+    }
+}
