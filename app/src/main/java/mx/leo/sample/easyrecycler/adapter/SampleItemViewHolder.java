539ffbfed723bf6e9dfd48a6c3ef1c0a560bf692
BraulioMendez <braauliomendez@ciencias.unam.mx>
2016-12-12 23:48:35 -0600

    NEW: Added Example To Library.

---
+++ app/src/main/java/mx/leo/sample/easyrecycler/adapter/SampleItemViewHolder.java
@@ -0,0 +1,33 @@
+package mx.leo.sample.easyrecycler.adapter;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import mx.leo.easyrecycler.viewholder.EasyItemViewHolder;
+import mx.leo.sample.easyrecycler.R;
+import mx.leo.sample.easyrecycler.model.Language;
+
+/**
+ * Created by Braulio on 12/12/2016.
+ */
+
+public class SampleItemViewHolder extends EasyItemViewHolder {
+
+    @BindView(R.id.sample_image_view) ImageView sampleImageView;
+
+    public SampleItemViewHolder(@NotNull View view) {
+        super(view);
+        ButterKnife.bind(this, view);
+    }
+
+    public void bindItem(Language language) {
+        Glide.with(sampleImageView.getContext()).load(language.getPhoto()).into(sampleImageView);
+    }
+}
+
